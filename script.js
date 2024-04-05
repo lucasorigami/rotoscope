90c334048533dd29cb87d0e33c6be6670fea3d60
Balint <korka.balint@gmail.com>
2024-04-05 23:30:11 +0200

    classes added

--- script.js
+++ script.js
@@ -8,37 +8,32 @@ fetch("https://opensheet.elk.sh/1wLuU7IKkp0cR2He09kU4lo84ue2qef_gUjvCsJUljmU/she
     .then((data) => {
         for (let item of data) {
             html += `
-    <h1><a href="" id="projectTitle">${item.Week}</a> </h1>
-    <div class="templateContainer" ${item.classname}>
+            <div class="Identifier"> 
+            <p>${item.Idendtifier} </p> 
   
-    <div class="descriptionContainer" >
-        <p>${item.Week} </p>
-    </div>
-    <div class="imageContainer"> 
-        <img src="${item.Description}"></img>
-    </div>
-    <div class="materialContainer"> 
-        <p>${item.Tasks} </p> 
-    </div>
-    <div class="yearContainer"> 
-        <p>${item.Todo} </p> 
-    </div>
-    <div class="yearContainer"> 
-    <p>${item.Feedback} </p> 
-    </div>
-    </div>
-    <div class="yearContainer"> 
-    <p>${item.Keywords} </p> 
-    </div>
-    </div>
-    <div class="yearContainer"> 
-    <p>${item.Image} </p> 
-    </div>
-    
-    <div class="sourceContainer" > 
-        <p><a href="${item.Source}" target="_blank">${item.Source}</a> </p>
-    </div>
-    </div>
+                 <div class="Week" >
+                    <p>${item.Week} </p>
+                 </div>
+                 <div class="Description"> 
+                    <img src="${item.Description}"></img>
+                </div>
+                <div class="Tasks"> 
+                    <p>${item.Tasks} </p> 
+                </div>
+                <div class="Todo"> 
+                    <p>${item.Todo} </p> 
+                </div>
+                <div class="Feedback"> 
+                    <p>${item.Feedback} </p> 
+                </div>
+                <div class="Keywords"> 
+                    <p>${item.Keywords} </p> 
+                </div>
+                <div class="Image"> 
+                    <p>${item.Image} </p> 
+                 </div>
+
+            </div>
     `;
         }
         output.innerHTML = html;
